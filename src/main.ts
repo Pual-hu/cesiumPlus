241337927df110a601d06f38982c684c0c1e70cb
hujialin <3504210788@qq.com>
2023-11-08 14:47:08 +0800

    feat: 📦添cesium文件静态资源，引入cesium

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+import "cesium/Build/Cesium/Widgets/widgets.css";
+(window as any).CESIUM_BASE_URL = 'src/assets';
 createApp(App).mount('#app')
